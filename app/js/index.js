705ac1fe4a88a164b5c7dba434e02080b3f27e97
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-16 15:21:56 +0300

    lesson 5

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,87 @@
 import "../scss/main.scss";
 
-window.onload = () => {};
+let canvas = document.querySelector(".game");
+/** @type {CanvasRenderingContext2D} */
+let ctx = canvas.getContext("2d");
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let ballRadius = 10;
+let dx = 3;
+let dy = -3;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleDx = 7;
+let leftpressed = false;
+let rightpressed = false;
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(e) {
+  if (e.code == "KeyA") {
+    leftpressed = true;
+  }
+  if (e.code == "KeyD") {
+    rightpressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.code == "KeyA") {
+    leftpressed = false;
+  }
+  if (e.code == "KeyD") {
+    rightpressed = false;
+  }
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+
+  x += dx;
+  y += dy;
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  }
+  if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert("GAME OVER");
+      document.location.reload();
+      clearInterval(interval); // Needed for Chrome to end game
+    }
+  }
+
+  paddleX += paddleDx * rightpressed - paddleDx * leftpressed;
+  if (paddleX + paddleWidth > canvas.width) {
+    paddleX = canvas.width - paddleWidth;
+  }
+  if (paddleX < 0) {
+    paddleX = 0;
+  }
+}
+let interval = setInterval(draw, 16);
